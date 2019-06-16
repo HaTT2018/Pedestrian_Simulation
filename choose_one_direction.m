de20bd6e7f883219dec0ebcff571b2c1cba20f99
HaTT2018 <38817831+HaTT2018@users.noreply.github.com>
2019-06-17 02:19:26 +0800

    Delete choose_one_direction.m

--- choose_one_direction.m
+++
@@ -1,8 +0,0 @@
-function alt_position = choose_one_direction(alt_position)
-    
-    pos_size = size(alt_position);
-    r = alt_position .* rand(pos_size);
-    r = r./max(r')';
-    r(r~=1)=0;
-    alt_position = r;
-end
